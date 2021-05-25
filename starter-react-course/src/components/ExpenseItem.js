56ae7efb950f3f55c100328a8975660e5ca2ec23
Vladimir Stajic <vstajic@novomind.com>
2021-05-25 21:10:01 +0200

    working on a date

--- starter-react-course/src/components/ExpenseItem.js
+++ starter-react-course/src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
- 
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+  const month = props.date.toLocaleString('en-US', {month: 'long'});
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
