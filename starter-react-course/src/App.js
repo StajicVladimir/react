31595f9168892dcfd47ee30e6d042f155359eb4b
Vladimir Stajic <vstajic@novomind.com>
2021-05-25 21:59:59 +0200

    good night

--- starter-react-course/src/App.js
+++ starter-react-course/src/App.js
@@ -25,7 +25,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses props={expenses}/>
+      <Expenses items={expenses}/>
     </div>
   );
 }
--- starter-react-course/src/components/Expenses.js
+++ starter-react-course/src/components/Expenses.js
@@ -1,30 +1,31 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
-        title={props.props[0].title}
-        amount={props.props[0].amount}
-        date={props.props[0].date}
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
       />
       <ExpenseItem
-        title={props.props[1].title}
-        amount={props.props[1].amount}
-        date={props.props[1].date}
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
       />
       <ExpenseItem
-        title={props.props[2].title}
-        amount={props.props[2].amount}
-        date={props.props[2].date}
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
       />
       <ExpenseItem
-        title={props.props[3].title}
-        amount={props.props[3].amount}
-        date={props.props[2].date}
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[2].date}
       />
-    </div>
+    </Card>
   );
 }
 
