b03e73e5fb04fe358a08fb592c344a067220c67d
Vladimir Stajic <vstajic@novomind.com>
2021-05-25 21:21:42 +0200

    extracting to sub component

--- starter-react-course/src/components/ExpenseItem.js
+++ starter-react-course/src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
+import './ExpenseDate';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-  const month = props.date.toLocaleString('en-US', {month: 'long'});
-  const year = props.date.getFullYear();
+  
   return (
     <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
