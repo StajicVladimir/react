15e71a8ef3f05af53d4ceca0d607807c686eb908
Vladimir Stajic <vstajic@novomind.com>
2021-05-28 15:46:23 +0200

    moving data from child to parent

--- starter-react-course/src/App.js
+++ starter-react-course/src/App.js
@@ -26,6 +26,10 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js!');
+    console.log(expense);
+  }
   // return React.createElement(
   //   'div', {},
   //   React.createElement('h2', {}, "Let 's get started"),
@@ -33,7 +37,7 @@ const App = () => {
   // );
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
