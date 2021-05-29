e03c9ed28bf6f924ad100362e8db128c7bf74d6b
Vladimir Stajic <vstajic@novomind.com>
2021-05-29 11:19:47 +0200

    dynamic lists

--- starter-react-course/src/components/Expenses/Expenses.js
+++ starter-react-course/src/components/Expenses/Expenses.js
@@ -1,42 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import "./Expenses.css";
 
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
-import ExpensesFilter from './ExpensesFilter';
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
-  const [selectedYear, setSelectedYear] = useState('2020');
+  const [selectedYear, setSelectedYear] = useState("2020");
 
   const selectYearHandler = (changedYear) => {
-    setSelectedYear(changedYear)
+    setSelectedYear(changedYear);
   };
 
   return (
     <div>
-      <ExpensesFilter selected={selectedYear} onSelectYear={selectYearHandler}/>
       <Card className="expenses">
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+        <ExpensesFilter
+          selected={selectedYear}
+          onSelectYear={selectYearHandler}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[2].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date} />
+        ))}
       </Card>
     </div>
   );
