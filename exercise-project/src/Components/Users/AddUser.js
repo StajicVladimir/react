8474711ce2d49f1afb0136317e2da83395ab5567
Vladimir Stajic <vstajic@novomind.com>
2021-06-01 21:29:15 +0200

    react fragment

--- exercise-project/src/Components/Users/AddUser.js
+++ exercise-project/src/Components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from '../Helpers/Wrapper';
 
 import classes from "./AddUser.module.css";
 
@@ -49,7 +50,7 @@ const AddUser = (props) => {
     setEnteredUserAge("");
   }
   return (
-    <div>
+    <Wrapper>
       {error && <ErrorModal title={error.title} content={error.content} onConfirm={errorHandler}/>}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
@@ -70,7 +71,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
