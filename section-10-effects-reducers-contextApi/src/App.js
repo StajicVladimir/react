476ac256c40777771f6324711db4b1b4638ff504
Vladimir Stajic <vstajic@novomind.com>
2021-06-02 21:09:18 +0200

    use effect for crappy login

--- section-10-effects-reducers-contextApi/src/App.js
+++ section-10-effects-reducers-contextApi/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,22 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storredLoginInfo = localStorage.getItem("isLoggedIn");
+    if(storredLoginInfo === '1') {
+      setIsLoggedIn('1');
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", '1');
   };
 
   const logoutHandler = () => {
+    localStorage.setItem("isLoggedIn", '0');
     setIsLoggedIn(false);
   };
 
