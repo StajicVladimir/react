fd6b4279bfef0ec32edced1142e2f2040c760740
Vladimir Stajic <vstajic@novomind.com>
2021-06-04 20:36:56 +0200

    food app yum yum

---
+++ food-app/src/Components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import HeaderCartButton from '../Layout/HeaderCartButton';
+import mealsImage from '../../asset/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = props => {
+    return (
+        <React.Fragment>
+            <header className = {classes.header}>
+                <h1>ReactMeals Yum Yum</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={['main-image']}>
+                <img src={mealsImage} alt='YUM YUM'></img>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default Header;
