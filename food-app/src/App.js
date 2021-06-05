345810989e0de058f0e419d2b922ec27a05f7e5f
Vladimir Stajic <vstajic@novomind.com>
2021-06-05 11:01:48 +0200

    added meals, cart, modal

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Cart from './Components/Cart/Cart';
 
 import Header from './Components/Layout/Header';
+import Meals from './Components/Meals/Meals';
+
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
