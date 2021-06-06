2919273a146a501bb169fca49864f05d3862b59c
Vladimir Stajic <vstajic@novomind.com>
2021-06-06 10:47:20 +0200

    adding and removing items in cart works

--- food-app/src/Components/Cart/Cart.js
+++ food-app/src/Components/Cart/Cart.js
@@ -11,8 +11,12 @@ const Cart = (props) => {
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
-  const cartItemRemoveHandler = () => {};
-  const cartItemAddHandler = () => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
 
   const cartItems = (
     <ul>
