1faaf476b81f1acfa752071cf81c448e81b65a8f
Vladimir Stajic <vstajic@novomind.com>
2021-06-06 11:50:14 +0200

    useCallback

--- section-12-a-look-behind-the-scenes/src/App.js
+++ section-12-a-look-behind-the-scenes/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Button from './components/UI/Button/Button';
 import './App.css';
@@ -7,9 +7,9 @@ import DemoOutput from './components/Demo/DemoOutput';
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
   console.log("App Component");
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  };
+  }, []);
   return (
     <div className="app">
       <h1>Hi there!</h1>
--- section-12-a-look-behind-the-scenes/src/components/UI/Button/Button.js
+++ section-12-a-look-behind-the-scenes/src/components/UI/Button/Button.js
@@ -21,4 +21,8 @@ const Button = (props) => {
 // sve se izvrsi ponovo, funkcija se ponovo napravi i iako radi sve isto
 //nije ista funkcija i zbog toga se props ovde promeni i svakako se renderuje opet
 //My paragraph se nece ponovo renderovati jer je boolean, String bla primitive type u JS
+
+//Kada se u App.js doda use Callback ovo se nece ponovo renderovati zato sto
+//useCallback ce da sacuva funkciju interno i onda ce ta funkcija svaki put kada se App.js ponovo izvrsi
+//zaista da bude ISTA funkcija (pokazuje u memoriji na isto mesto), pa se onda ni props ovde nisu promenili
 export default React.memo(Button);
