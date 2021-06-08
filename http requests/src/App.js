424074923cf8fbd38ba03e92dcabb57767aedbfe
Vladimir Stajic <vstajic@novomind.com>
2021-06-08 21:13:25 +0200

    make it a bit prettier

--- http requests/src/App.js
+++ http requests/src/App.js
@@ -36,16 +36,26 @@ function App() {
     setIsLoading(false);
   }
 
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if(error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>No loaded Movies</p>}
-        {isLoading && <p>Loading...</p>}
-        {!isLoading && error && <p>{error}</p>}
+        {content}
       </section>
     </React.Fragment>
   );
