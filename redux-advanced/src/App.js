181f45308b1d952966511f45e3ff956c6e2cf810
Vladimir Stajic <vstajic@novomind.com>
2021-07-17 11:56:36 +0200

    fetching data from firebase in component... next remove that and use action THUNK

--- redux-advanced/src/App.js
+++ redux-advanced/src/App.js
@@ -1,15 +1,79 @@
-import { useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useSelector, useDispatch } from "react-redux";
+import { Fragment, useEffect } from "react";
+import { uiActions } from "./store/ui-slice";
+
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+  useEffect(() => {
+    const sentCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+
+      const response = await fetch(
+        "https://react-http-e8e8e-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed!");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    sentCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Error while sending data!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
