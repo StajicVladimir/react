a135d5e77d5c498f1e21cd9d1037dff511142536
Vladimir Stajic <vstajic@novomind.com>
2021-07-17 11:01:29 +0200

    redux exercise shop

--- redux-advanced/src/components/Shop/Products.js
+++ redux-advanced/src/components/Shop/Products.js
@@ -1,17 +1,26 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {id: 'p1', price: 6, title: 'My First Book', description: 'Fooo desc'},
+  {id: 'p2', price: 10, title: 'MySecond book', description: 'Baaaar'}
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
         />
-      </ul>
+        ))}
+              </ul>
     </section>
   );
 };
