f38fafd8a81b0a2f9b4d2afe49832f29857f0249
Vladimir Stajic <vstajic@novomind.com>
2021-07-18 21:54:33 +0200

    routing exercise wrapped layuot nav around app

--- routing exercise/src/App.js
+++ routing exercise/src/App.js
@@ -3,22 +3,27 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuotes from "./components/pages/AllQuotes";
 import QuoteDetail from "./components/pages/QuoteDetail";
 import NewQuote from "./components/pages/NewQuote";
+import Layout from "./components/layout/Layout";
 
 function App() {
-  return (<Switch>
-    <Route path="/" exact>
-      <Redirect to="/quotes"/>
-    </Route>
-    <Route path='/quotes' exact>
-      <AllQuotes />
-    </Route>
-    <Route path='/quotes/:quoteId'>
-      <QuoteDetail />
-    </Route>
-    <Route path='/new-quote'>
-      <NewQuote />
-    </Route>
-  </Switch>);
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
+  );
 }
 
 export default App;
