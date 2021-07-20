83f978933279ab2da252337160a255d50c66b964
Vladimir Stajic <vstajic@novomind.com>
2021-07-20 21:15:10 +0200

    added lazy loading of js code

--- routing exercise/src/App.js
+++ routing exercise/src/App.js
@@ -1,31 +1,42 @@
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 
-import AllQuotes from "./components/pages/AllQuotes";
-import QuoteDetail from "./components/pages/QuoteDetail";
-import NewQuote from "./components/pages/NewQuote";
 import Layout from "./components/layout/Layout";
-import NotFound from "./components/pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import("./components/pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./components/pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./components/pages/NotFound"));
+const AllQuotes = React.lazy(() => import("./components/pages/AllQuotes"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
