60959530fb43ad150ead7c160cd89085b525f8c2
Vladimir Stajic <vstajic@novomind.com>
2021-07-24 12:20:46 +0200

    typescript start - basics

---
+++ react-typescript/basics.ts
@@ -0,0 +1,12 @@
+// Primitives ** lowercase **
+// numbes, string, boolean
+
+//number int and float
+let age: number;
+age = 12;
+
+let userName: string;
+userName = 'Max';
+
+let isInstructor: boolean;
+isInstructor = true;
