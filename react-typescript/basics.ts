897b3d122bddeb23555eec0e7cdbdeafc335b0a4
Vladimir Stajic <vstajic@novomind.com>
2021-07-24 12:25:28 +0200

    typescript base types - objects

--- react-typescript/basics.ts
+++ react-typescript/basics.ts
@@ -10,3 +10,29 @@ userName = 'Max';
 
 let isInstructor: boolean;
 isInstructor = true;
+
+
+// more complex types
+
+//array
+let hobbies: string[];
+hobbies = ['Sports', 'Cooking'];
+
+//objects
+
+let person: {
+    name: string;
+    age: number;
+};
+
+person = {
+    name: 'Max',
+    age: 32
+};
+
+//array of complex objects
+
+let people: {
+    name: string;
+    age: number;
+}[];
