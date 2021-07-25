7033296763e2772914fbd41ae2273b14f23ef3cb
Vladimir Stajic <vstajic@novomind.com>
2021-07-25 11:00:42 +0200

    still on basics with typescript

--- react-typescript/react-ts/src/App.tsx
+++ react-typescript/react-ts/src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import './App.css';
+import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn Typescript')
+  ];
   return (
-    <div className="App">
+    <div>
+      <Todos items={todos}/>
     </div>
   );
 }
