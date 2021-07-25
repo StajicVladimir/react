75872cd7e9364b8acbcf7d26fb7028f679fd7aa5
Vladimir Stajic <vstajic@novomind.com>
2021-07-25 11:38:14 +0200

    using state for managing todos

---
+++ react-typescript/react-ts/src/components/NewTodo.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { useRef } from "react";
+
+const NewTodo:React.FC<{onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    
+    const enteredText = todoTextInputRef.current!.value;
+
+    if(enteredText.trim().length === 0){
+        //TODO throw an error
+        return;
+    }
+
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" ref={todoTextInputRef}></input>
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
